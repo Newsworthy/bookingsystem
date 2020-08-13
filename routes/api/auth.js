ce48daab5b6a0838e8df8f7568ad2b7d526f526c
Nick Smith <development@newsworthyision.com>
2020-08-13 20:21:22 +0100

    Getting closer...

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,59 +4,60 @@ const bcrypt = require('bcryptjs');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 const auth = require('../../middleware/auth');
-
-const User = require('../../models/User')
+// User model
+const User = require('../../models/User');
 
 // @route POST api/auth
 // @desc AUTH user
-// @access Public
+// @access  Public
 
 router.post('/', (req, res) => {
-    const { email, password } = req.body;
-    //Validation
-    if (!email || !password) {
-        return res.status(400).json({ msg: 'Please enter all fields' });
-    }
-    //Check for existing user
-    User.findOne({ email })
-        .then(user => {
-            if (!user) return res.status(400).json({ msg: 'User does not exist' });
+  const { email, password } = req.body;
+    console.log("Test");
+  // Simple Validation
+  if(!email || !password) {
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
 
-            // Validate Password
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+  // Check for existing user
+  User.findOne({ email })
+    .then(user => {
+      if(!user) return res.status(400).json({ msg: 'User does not exist' });
 
-                    jwt.sign(
-                        { id: user.id },
-                        config.get('jwtSecret'),
-                        { expiresIn: 3600 },
-                        (err, token) => {
-                            if (err) throw err;
-                            res.json({
-                                token,
-                                user: {
-                                    id: user.id,
-                                    name: user.name,
-                                    email: user.email
-                                }
-                            });
-                        }
-                    )
-                    console.log("User " + user.name + " has just logged in.");
-                })
+      // Validate password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+
+          jwt.sign(
+            { id: user.id },
+            config.get('jwtSecret'),
+            { expiresIn: 3600 },
+            (err, token) => {
+              if(err) throw err;
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email
+                }
+              });
+            }
+          )
         })
-        
+
+    })
 });
 
-// @route GET /api/auth/user
+// @route GET api/auth/user
 // @desc GET user data
-// @access Private
+// @access  Private
 
 router.get('/user', auth, (req, res) => {
-    User.findById(req.user.id)
-        .select('-password')
-        .then(user => res.json(user));
+  User.findById(req.user.id)
+    .select('-password')
+    .then(user => res.json(user));
 });
 
 module.exports = router;
